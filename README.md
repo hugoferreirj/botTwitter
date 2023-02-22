c39c090fb29b41fd5f18077a6a8d12e4692e848b
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-02-22 18:45:45 -0300

    atualização no intervalo de postagens e link da musica no youtube

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # botTwitter
-Esse projeto consiste em um bot que diariamente posta um tweet com o verso de uma música sobre amizade e marca um dos meus amigos.
+Esse projeto consiste em um bot que semanlmente posta um tweet com o verso de uma música sobre amizade e marca um dos meus amigos. Após postar o verso ele publica o link da música no youtube em resposta.
 
 O bot foi desenvolvido em python utilizando a API Tweepy. As letras das músicas são lidas de um arquivo JSON.
 
--- bottwitter.py
+++ bottwitter.py
@@ -3,12 +3,19 @@ import tweepy
 import time
 import random
 import json
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+
 # api twitter
 
 auth = tweepy.OAuthHandler(os.environ.get('API_KEY'), os.environ.get('API_SECRET_KEY'))
 auth.set_access_token(os.environ.get('ACESS_KEY'), os.environ.get('ACESS_SECRET'))
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# api youtube
+keyYT = os.environ.get('YOUTUBE_KEY')
+
 
 # abre e lê arquivo json
 arq = open('letrasDasMusicas.json',  encoding='utf-8')
@@ -17,11 +24,11 @@ data = json.load(arq)
 
 def sorteiaMusica():
     musicaEscolhida = random.choice(data['Musicas'])
-    estrofesComRepeticao = musicaEscolhida["Letra"].split("\n\n")
-    return estrofesComRepeticao
+    return musicaEscolhida
 
 # elimina estrofes repetidas e escolhe uma estrofe para se postada
-def sorteiaEstrofe(estrofesComRepeticao):
+def sorteiaEstrofe(musicaEscolhida):
+    estrofesComRepeticao = musicaEscolhida["Letra"].split("\n\n")
     estrofesUnicas = list(set(estrofesComRepeticao))
     estrofeEscolhida = random.choice(estrofesUnicas)
     return estrofeEscolhida
@@ -34,18 +41,27 @@ def escolheAmigo():
 
 # pega o id do ultimo tweet feito pela conta para responder em thread
 
-
 def pegaIdUltimoTweet():
     tweets_list = api.user_timeline(screen_name="botdohugo", count=1)
     tweet = tweets_list[0]
     return tweet.id
 
+# posta um tweet com o link da musica no youtube em resposta
+def postaLinkYoutube(musica):
+    id = pegaIdUltimoTweet()
+    buscaYoutube = musica["Nome"] + " " + musica["Cantor"]
+    try:
+        linkYoutube = pegaVideo(buscaYoutube)
+        tweet = "Você pode escutar essa música aqui: " + linkYoutube
+        api.update_status(status=tweet, in_reply_to_status_id=id)
+    except HttpError as e:
+        print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
 
 def postaTweet():
     user = escolheAmigo()
     musica = sorteiaMusica()
     estrofe = sorteiaEstrofe(musica)
-    tweet = ".@" + user + " " + estrofe
+    tweet = "." + user + " " + estrofe
     tamanhoTweet = len(tweet)
     if (tamanhoTweet > 279):
         api.update_status(tweet[0:279])  # posta primeiro tweet, sem ser uma reply
@@ -58,8 +74,27 @@ def postaTweet():
             i += 279
     else:
         api.update_status(tweet)
+    postaLinkYoutube(musica)
+    
+
+
+# procura musica no youtube e retorna o link
+def pegaVideo(busca):
+    youtube = build('youtube', 'v3',
+    developerKey=keyYT)
+
+    search_response = youtube.search().list(
+    q=busca,
+    part="id,snippet",
+    maxResults=1,
+    type="video",
+    videoEmbeddable="true").execute()
+
+    videos = search_response.get("items", [])
+    link = "https://youtu.be/" + videos[0]["id"]["videoId"]
+    return link
 
 
 while True:
     postaTweet()
-    time.sleep(82800)
+    time.sleep(6*82800)
